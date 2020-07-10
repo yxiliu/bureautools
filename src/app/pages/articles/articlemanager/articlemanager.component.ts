5e8dac2d52e0b07bfd16074458646c1e511dacbf
yxiliu <yxiliu@yeah.net>
2020-07-10 22:06:49 +0800

    t

--- src/app/pages/articles/articlemanager/articlemanager.component.ts
+++ src/app/pages/articles/articlemanager/articlemanager.component.ts
@@ -4,7 +4,14 @@ import { NzMessageService } from 'ng-zorro-antd/message';
 
 const count = 10;
 const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
-
+interface Iinfodata{
+  title: string;
+  maintainer: string;
+  id: number;
+  tag: string;
+  date: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-articlemanager',
@@ -15,20 +22,30 @@ export class ArticlemanagerComponent implements OnInit {
   initLoading = true; // bug
   loadingMore = false;
   data: any[] = [];
-  list: Array<{ loading: boolean; name: any }> = [];
+  list: Array<Iinfodata> = [];
 
   constructor(private http: HttpClient, private msg: NzMessageService) {}
 
   ngOnInit(): void {
     this.getData((res: any) => {
-      this.data = res.results;
-      this.list = res.results;
+    
+      this.data = res;
+      this.list = res;
       this.initLoading = false;
     });
   }
 
   getData(callback: (res: any) => void): void {
-    this.http.get(fakeDataUrl).subscribe((res: any) => callback(res));
+    // this.http.get(fakeDataUrl).subscribe((res: any) => callback(res));
+    let fakeres = [
+      {'id':1, 'title':'大气相关的', "tag":"daq", "maintainer":'yxiliu',"date":'2019-01-02', code:'京发102号'},
+      {'id':1, 'title':'大气相关的', "tag":"daq", "maintainer":'yxiliu',"date":'2019-01-02',code:'京发102号'},
+      {'id':1, 'title':'大气相关的', "tag":"daq", "maintainer":'yxiliu',"date":'2019-01-02',code:'京发102号'},
+      {'id':1, 'title':'大气相关的', "tag":"daq", "maintainer":'yxiliu',"date":'2019-01-02',code:'京发102号'},
+      {'id':1, 'title':'大气相关的', "tag":"daq", "maintainer":'yxiliu',"date":'2019-01-02',code:'京发102号'},
+      {loading:true, 'id':1, 'title':'大气相关的', "tag":"daq", "maintainer":'yxiliu',"date":'2019-01-02',code:'京发102号'},
+    ]
+    callback(fakeres)
   }
 
   onLoadMore(): void {

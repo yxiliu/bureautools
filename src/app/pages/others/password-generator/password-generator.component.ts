56a967cd495f77faf6643d76efba6a9bf7197108
yxiliu <yxiliu@yeah.net>
2020-07-14 22:42:37 +0800

    t

--- src/app/pages/others/password-generator/password-generator.component.ts
+++ src/app/pages/others/password-generator/password-generator.component.ts
@@ -10,16 +10,47 @@ export class PasswordGeneratorComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    console.log(this.generator(2, 20))
   }
 
-  Upper: Array<string> = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  lower: Array<string> = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  num: Array<string> = ['0','1','2','3','4','5','6','7','8','9',]
-  spe: Array<string> = ['_','-','$','%','&','@','+','!']
-  
-  generator(type:number):string{
-    let randomGenerator = (n:number)=>Math.floor(Math.random()*n)
-    randomGenerator(4)
-    return "dasf"
+  haveSpe: boolean = true;
+  haveNum: boolean = true;
+  haveUpper: boolean = true;
+  haveLower: boolean = true;
+  Upper: Array<string> = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  lower: Array<string> = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  num: Array<string> = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
+  spe: Array<string> = ['_', '-', '$', '%', '&', '@', '+', '!']
+
+
+  validate(): boolean {
+    return this.haveSpe || this.haveNum || this.haveUpper || this.haveLower
+  }
+
+  leastCheck(list:Array<number>):Array<number>{
+    if (this.haveUpper && !(0 in list)){
+      
+    }
+    return 
+  }
+
+  generator(type: number, length: number): string {
+    if (this.validate()) {
+      let combineList: Array<Array<string>> = [this.Upper, this.lower, this.num, this.spe]
+      let transformData = { 0: combineList[0].length, 1: combineList[1].length, 2: combineList[1].length, 3: combineList[1].length }
+      let randomGenerator = (n: number) => Math.floor(Math.random() * n)
+      let arrayofrandom: Array<string> = []
+
+      for (let a = 0; a < length; a++) {
+        let firstRandom = randomGenerator(4)
+        arrayofrandom.push(combineList[firstRandom][randomGenerator(transformData[firstRandom])])
+      }
+
+      return arrayofrandom.join('')
+    } else {
+      return
+    }
+
   }
 }

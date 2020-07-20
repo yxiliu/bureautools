90f815b0f243723a7e2bbe158a5e1edb50b483ad
yxiliu <yxiliu@yeah.net>
2020-07-20 13:39:43 +0800

    t

--- src/app/pages/missions/missions-routing.module.ts
+++ src/app/pages/missions/missions-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import {ManagerComponent} from "./manager/manager.component";
+import {EditComponent} from "./edit/edit.component";
+import { ProgressComponent} from "./progress/progress.component";
+const routes: Routes = [
+  {path:'',component:ManagerComponent},
+  {path:"progress",component:ProgressComponent},
+  {path:"edit",component:EditComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
